b3e9222ed5e33a01436521869cdbc7eb8ee11ca1
alexodorico <alex.odorico@morleynet.com>
2019-08-22 19:54:03 -0400

    Have cloudinary uploader working

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const auth = require("./auth");
 const uploader = require("./uploader");
 const cloudinary = require('cloudinary').v2;
 const port = process.env.PORT || 3000;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_URL,
+  cloud_name: process.env.CLOUDINARY_CLOUD,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET
 });
 
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/upload/post/:userId", auth.verifyToken, uploader.postUpload);
+app.post("/upload/post/:postId", auth.verifyToken, uploader.postUpload);
 app.post("/upload/user/:userId", auth.verifyToken, uploader.userAvatarUpload);
 app.post("/upload/cat/:catId", auth.verifyToken, uploader.catAvatarUpload);
 
--- uploader.js
+++ uploader.js
@@ -1,19 +1,51 @@
+const vision = require('@google-cloud/vision');
 const cloudinary = require('cloudinary').v2;
+const client = new vision.ImageAnnotatorClient();
 const db = require("./db");
 
 const postUpload = async (req, res) => {
+  const { secure_url } = await cloudinary.uploader.upload(req.body.data, (error, result) => console.log(result));
+  const success = await updateDb("posts", "photo_url", secure_url, req.params.postId);
 
+  if (success) {
+    return res.status(201).json({ success: true, url: secure_url });
+  } else {
+    return res.status(400).json({ success: false });
+  }
 }
 
 const userAvatarUpload = async _ => {
+  const { secure_url } = await cloudinary.uploader.upload(req.body.data, (error, result) => console.log(result));
+  const success = await updateDb("users", "avatar_url", url, req.params.userId);
 
+  if (success) {
+    return res.status(201).json({ success: true, url: secure_url });
+  } else {
+    return res.status(400).json({ success: false });
+  }
 }
 
 const catAvatarUpload = async _ => {
+  const { secure_url } = await cloudinary.uploader.upload(req.body.data, (error, result) => console.log(result));
+  const success = await updateDb("cats", "avatar_url", url, req.params.catId);
 
+  if (success) {
+    return res.status(201).json({ success: true, url: secure_url });
+  } else {
+    return res.status(400).json({ success: false });
+  }
 }
 
+async function updateDb(table, column, url, id) {
+  const query = `UPDATE ${table} SET ${column} = $1 WHERE id = $2`;
 
+  try {
+    await db.query(query, [url, id]);
+    return true
+  } catch (error) {
+    return false
+  }
+}
 
 module.exports = {
   postUpload,
