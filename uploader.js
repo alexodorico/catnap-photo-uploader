8e3918053816edc802c1d4c04c4c8a8268460aad
alexodorico <alex.odorico@morleynet.com>
2019-08-22 22:00:39 -0400

    Cat photos are successfuly uploaded

--- uploader.js
+++ uploader.js
@@ -1,16 +1,44 @@
 const vision = require('@google-cloud/vision');
-const cloudinary = require('cloudinary').v2;
 const client = new vision.ImageAnnotatorClient();
+const cloudinary = require('cloudinary').v2;
+const fs = require("fs");
 const db = require("./db");
 
-const postUpload = async (req, res) => {
-  const { secure_url } = await cloudinary.uploader.upload(req.body.data, (error, result) => console.log(result));
-  const success = await updateDb("posts", "photo_url", secure_url, req.params.postId);
+const catUpload = async (req, res) => {
+  const error = res.status(400).json({
+    success: false,
+    message: "Oops! Something went wrong while trying to post your photo."
+  });
 
-  if (success) {
-    return res.status(201).json({ success: true, url: secure_url });
-  } else {
-    return res.status(400).json({ success: false });
+  try {
+    const file = req.file.path;
+    const [result] = await client.labelDetection(file);
+    const annotations = result.labelAnnotations;
+    const cat = checkForCat(annotations);
+
+    if (cat) {
+      const secure_url = await upload(file);
+
+      if (secure_url) {
+        const success = await updateDb("posts", "photo_url", secure_url, req.params.postId);
+
+        if (success) {
+          deleteFile(file);
+          return res.status(201).json({ success: true, url: secure_url });
+        }
+
+        deleteFile(file);
+        return error;
+      }
+
+      deleteFile(file);
+      return error;
+    }
+
+    deleteFile(file);
+    return res.status(400).json({ success: false, message: "Please upload a picture of a cat!" });
+  } catch (e) {
+    return error;
   }
 }
 
@@ -25,18 +53,28 @@ const userAvatarUpload = async _ => {
   }
 }
 
-const catAvatarUpload = async _ => {
-  const { secure_url } = await cloudinary.uploader.upload(req.body.data, (error, result) => console.log(result));
-  const success = await updateDb("cats", "avatar_url", url, req.params.catId);
+const checkForCat = annotations => {
+  for (label of annotations) {
+    if (label.description === "Cat") {
+      return true;
+      break;
+    }
 
-  if (success) {
-    return res.status(201).json({ success: true, url: secure_url });
-  } else {
-    return res.status(400).json({ success: false });
+    return false;
   }
 }
 
-async function updateDb(table, column, url, id) {
+const upload = async file => {
+  const { secure_url } = await cloudinary.uploader.upload(file);
+
+  if (secure_url) {
+    return secure_url;
+  }
+
+  return false;
+}
+
+const updateDb = async (table, column, url, id) => {
   const query = `UPDATE ${table} SET ${column} = $1 WHERE id = $2`;
 
   try {
@@ -47,8 +85,15 @@ async function updateDb(table, column, url, id) {
   }
 }
 
+const deleteFile = file => {
+  fs.unlink(file, err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
 module.exports = {
-  postUpload,
+  catUpload,
   userAvatarUpload,
-  catAvatarUpload
 }
