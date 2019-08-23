0ab54cfafe15e5543a50491cba774d06e6b61b64
alexodorico <alex.odorico@morleynet.com>
2019-08-22 22:00:02 -0400

    Added multer middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 const auth = require("./auth");
 const uploader = require("./uploader");
 const cloudinary = require('cloudinary').v2;
@@ -14,13 +15,17 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET
 });
 
+const upload = multer({
+  dest: "./uploads"
+});
+
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/upload/post/:postId", auth.verifyToken, uploader.postUpload);
-app.post("/upload/user/:userId", auth.verifyToken, uploader.userAvatarUpload);
-app.post("/upload/cat/:catId", auth.verifyToken, uploader.catAvatarUpload);
+app.post("/api/upload/post/:postId", auth.verifyToken, upload.single("file"), uploader.catUpload);
+app.post("/api/upload/user/:userId", auth.verifyToken, upload.single("file"), uploader.userAvatarUpload);
+app.post("/api/upload/cat/:catId", auth.verifyToken, upload.single("file"), uploader.catUpload);
 
 app.listen(port, _ => console.log(`Listenin' on port ${port} 🔥`));
